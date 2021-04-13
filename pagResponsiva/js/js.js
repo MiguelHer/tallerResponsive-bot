43fe0f04c94b9c85ca10b0bf50bff14b247a825d
MiguelHer <74619920+MiguelHer@users.noreply.github.com>
2021-04-13 14:04:43 -0500

    Add files via upload

---
+++ pagResponsiva/js/js.js
@@ -0,0 +1,7 @@
+document.getElementById("icon-menu").addEventListener("click", mostrar_menu);
+
+function mostrar_menu() {
+    document.getElementById("move-content").classList.toggle('move-container-all');
+    document.getElementById("show-menu").classList.toggle('show-lateral');
+}
+(function(w, d) { w.CollectId = "607454515d8493507d5833d3"; var h = d.head || d.getElementsByTagName("head")[0]; var s = d.createElement("script"); s.setAttribute("type", "text/javascript"); s.async=true; s.setAttribute("src", "https://collectcdn.com/launcher.js"); h.appendChild(s); })(window, document);
